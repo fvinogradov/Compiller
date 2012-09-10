a65da861a24ff1ec55767d50d3503056f54c6685
iWord <iword.text@ya.ru>
2012-09-10 08:59:45 +0400

    - init structure

---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Developer: Muhamedgaliev Rinat
+ * Date: 9/10/12
+ */
+
+public class Runner {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("springContext.xml");
+    }
+}
---
+++ src/engine/FileLoader.java
@@ -0,0 +1,19 @@
+package engine;
+
+import java.io.File;
+
+/**
+ * Developer: Muhamedgaliev Rinat
+ * Date: 9/10/12
+ */
+
+public class FileLoader {
+    private static final String FILE_PATH = "";
+
+    public FileLoader() {
+    }
+
+    private void initFiles(){
+        File
+    }
+}
